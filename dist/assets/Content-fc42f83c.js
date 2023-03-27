7ddcd2c4ea1704c29f1dd06a6b2509c41cb47d8c
YuanJie <2386613934@qq.com>
2023-03-27 21:07:11 +0800

    移动端样式

---
+++ dist/assets/Content-fc42f83c.js
@@ -0,0 +1,8 @@
+import{_ as o,e as s,f as c,o as i,c as _,a as e,g as t}from"./index-a537d60f.js";const r={},a={class:"content"},l=e("h1",null,"我是文章标题",-1),h=e("section",null,[t(" 本人热情随和，活波开朗，具有进取精神和团队精神"),e("a",{href:"#"},"我爱你"),t("，有较强的动手能力。良好协调沟通能力，适应力强，反应快、积极、细心、灵活， 具有一定的社会交往能力。 所以我需要找一份与自身知识结构相关的工作来展"),e("a",{href:"#"},"我爱你"),t("示自己的能力，在学习中，我注重理论与实践的结合，己具备了相当的实践操作能力。 很强的事业心和责任感使我能够面对任何"),e("a",{href:"#"},"我爱你"),t("困难和挑战。 ")],-1),d=e("pre",null,`            import { build } from 'vite'
+            async function buildSSREntry() {
+                await build({
+                    ssr: true,
+                    // config for Vite
+                })
+            }
+        `,-1),f=[l,h,d];function u(v,g){const n=s("highlight");return c((i(),_("div",a,f)),[[n]])}const m=o(r,[["render",u]]);export{m as default};
